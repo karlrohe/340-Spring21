79c7af7ad1fa71d28f09a8a85d5192dfa808e033
Bi Cheng Wu <bwu62@wisc.edu>
2020-11-26 13:38:41 -0600

    tweaked plot line thickness

--- data/bls/plot.R
+++ data/bls/plot.R
@@ -31,19 +31,19 @@ unemployment %<>% mutate(id = as.character(paste0(stateFIPS,countyFIPS)),
 #   ggtitle("US unemployment by county (2019)") + theme(plot.title=element_text(hjust=.5,size=16))
 
 # animated plot (may require latest developmental version of ggplot2/gganimate from github)
-p1 = ggplot(unemployment, aes(map_id=id)) + theme_void() + coord_fixed() + 
+p = ggplot(unemployment, aes(map_id=id)) + theme_void() + coord_fixed() + 
   geom_map(aes(fill=unemployment_rate),map=county_map_data,color="black",size=.1) + 
   expand_limits(x=county_map_data$long,y=county_map_data$lat) + 
-  geom_map(data=state_map_data,aes(group=group),map=state_map_data,fill=NA,color="black",size=.2) + 
+  geom_map(data=state_map_data,aes(group=group),map=state_map_data,fill=NA,color="black",size=.5) + 
   scale_fill_continuous(type="viridis",trans='log10',
                         breaks=B<-c(.7,1,1.5,2,2.5,3,4,5,6,8,10,12,14,16,18,20),labels=B,
                         limits=c(0.7,20)) + 
   guides(fill = guide_colourbar(barwidth=1,barheight=20,title="Rate (%)",
                                 title.theme=element_text(margin=unit(c(0,0,12,0),"pt")))) + 
-  ggtitle("US unemployment by county {current_frame}") + 
+  ggtitle("US unemployment by county ({current_frame})") + 
   theme(plot.title=element_text(hjust=.5,size=16)) + 
   transition_manual(year)
 
 animate(p,nframes=length(unique(unemployment$year))+6,fps=2,
-        width=8.6,height=5.8,units='in',res=150,start_pause=1,end_pause=5)
+        width=9,height=6,units='in',res=150,start_pause=1,end_pause=5)
 anim_save("unemployment.gif")
