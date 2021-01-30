b7e3aecf13503d7b55e358bea93586af7afcf16c
karl rohe <karlrohe@karls-MacBook-Pro.local>
2021-01-30 09:08:08 -0600

    put the function get_bls_county() directly into the code (instead of sourcing it).

--- data/bls/recent_county_months.R
+++ data/bls/recent_county_months.R
@@ -1,4 +1,92 @@
 # downloads all county data for recent months:
 
-source("https://raw.githubusercontent.com/karlrohe/blscrapeR/master/get_bls_county.R")
+# this function originally appeared in the package blscrapeR.  I've edited it to keep "all the data downloaded".
+
+get_bls_county <- function(){
+  # Set some dummy variables. This keeps CRAN check happy.
+  countyemp=contyemp=fips_state=V1=V2=V3=V4=V5=V6=V7=V8=V9=period=i=unemployed=employed=labor_force=NULL
+  state_fips <- blscrapeR::state_fips
+  # See if URL is available
+  target <- blscrapeR::urlExists("https://www.bls.gov/web/metro/laucntycur14.txt")
+  if(!isTRUE(target)){
+    message("Woops, looks like 'https://www.bls.gov/web/metro/laucntycur14.txt' is unavailable right now!")
+  } else {
+    temp<-tempfile()
+    download.file("https://www.bls.gov/web/metro/laucntycur14.txt", temp)
+  }
+  countyemp <- read.csv(temp, fill=T, header=F, sep="|", skip=6, stringsAsFactors=F, strip.white=T) %>% 
+    dplyr::rename(area_code=V1, fips_state=V2, fips_county=V3, area_title=V4, period=V5, labor_force=V6, employed=V7, 
+                  unemployed=V8, unemployed_rate=V9) %>%
+    # Get rid of empty rows at the bottom and set period to proper date format.
+    na.omit() %>% dplyr::mutate(period=as.Date(paste("01-", period, sep = ""), format = "%d-%b-%y"))
+  
+  # Get the FIPS code: Have to add leading zeros to any single digit number and combine them.
+  countyemp$fips_county <- formatC(countyemp$fips_county, width = 3, format = "d", flag = "0")
+  countyemp$fips_state <- formatC(countyemp$fips_state, width = 2, format = "d", flag = "0")
+  countyemp$fips <- paste(countyemp$fips_state,countyemp$fips_county,sep="")
+  
+  unlink(temp)
+  
+  # # Check to see if user selected specific state(s).
+  # if (!is.null(stateName)){
+  #     # Check to see if states exists.
+  #     state_check <- purrr::map_lgl(stateName, function(x) any(grepl(x, state_fips$state)))
+  #     if(any(state_check==FALSE)){
+  #         stop(message("Please make sure you state names are spelled correctly using full state names."))
+  #     }
+  #     # If state list is valid. Grab State FIPS codes from internal data set and subset countyemp
+  #     state_rows <- purrr::map_int(stateName, function(x) grep(x, state_fips$state))
+  #     state_selection <- state_fips$fips_state[state_rows]
+  #     
+  #     statelist <- purrr::map(state_selection, function(s){
+  #         state_vals <- subset(countyemp, fips_state==s)
+  #     })
+  #     
+  #     countyemp <- do.call(rbind, statelist)
+  # }
+  # 
+  # # Check for date or dates.
+  # if (!is.null(date_mth)){
+  #     date_mth <- as.Date(paste("01", date_mth, sep = ""), format = '%d %b %Y')
+  #     # Check to see if users date exists in data set.
+  #     dt_exist <- sapply(date_mth, function(x) any(grepl(x, countyemp$period)))
+  #     if(any(dt_exist==FALSE)){
+  #         message("Are you sure your date(s) is published? Please check the BLS release schedule.")
+  #         if(i>Sys.Date()-54){
+  #             stop(message("County-wide statistics are usually published on the third Friday of each month for the previous month."))
+  #         }
+  #         if(i<Sys.Date()-360){
+  #             stop(message("This data set only goes back one year. Make sure your date(s) is correct."))
+  #         }
+  #     }
+  # }
+  # 
+  # if (is.null(date_mth)){
+  #     date_mth <- max(countyemp$period)
+  #     date_mth <- as.Date(date_mth, format = '%d %b %Y')
+  # }
+  
+  # # Put months to loop in list.
+  # datalist <- purrr::map(date_mth, function(i){
+  #     mth_vals <- subset(countyemp, period==i)
+  # })
+  # 
+  # Rebind.
+  
+  # Correct column data types.
+  # remove PR (no data)
+  df = countyemp %>% 
+    dplyr::filter(fips_state != 72) %>% 
+    dplyr::mutate(unemployed_rate = as.numeric(unemployed_rate),
+                  unemployed=as.numeric(gsub(",", "", as.character(unemployed))), 
+                  employed=as.numeric(gsub(",", "", as.character(employed))),
+                  labor_force=as.numeric(gsub(",", "", as.character(labor_force)))) %>% 
+    tibble::as_tibble()
+  return(df)
+  
+}
+
+
+
+
 df = get_bls_county()
